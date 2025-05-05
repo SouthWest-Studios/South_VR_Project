2e307b833cd8cae010bff8d59c9ed870b32316e0
Aleix3 <Aleixbotella.1@gmail.com>
2025-05-05 20:30:41 +0200

    initial black jack minigame

---
+++ Humble Gamble/Assets/South/Scripts/BlackJackCardScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackJackCardScript : MonoBehaviour
+{
+
+    public enum CardType
+    {
+        Treboles = 0,
+        Picas = 1,
+        Corazones = 2,
+        Diamantes = 3
+    }
+
+    public int id;
+    public int value;
+    public CardType type;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
