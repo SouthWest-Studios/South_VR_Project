37ece89227e5de7948689e0a32e8e585e715911d
Aleix3 <Aleixbotella.1@gmail.com>
2025-05-12 09:58:23 +0200

    spade cards spawning done

--- HumbleGamblev2/Assets/South/Scripts/BlackJack/BlackJackCardScript.cs
+++ HumbleGamblev2/Assets/South/Scripts/BlackJack/BlackJackCardScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BlackJackCardScript : MonoBehaviour
 {
+    Transform childTransform;
+    BlackJackScript blackJack; 
 
     public enum CardType
     {
@@ -19,12 +22,53 @@ public class BlackJackCardScript : MonoBehaviour
 
     void Start()
     {
-        
+        blackJack = FindAnyObjectByType<BlackJackScript>();
+        childTransform = transform.Find("Face");
+
+        MeshRenderer meshRenderer = childTransform.GetComponent<MeshRenderer>();
+
+        if (meshRenderer != null)
+        {
+            Texture2D texture = null;
+
+            if (id >= 1 && id <= 10)
+            {
+                texture = blackJack.spadesTextures[id - 1];
+            }
+            else if (id == 11) // J
+            {
+                texture = blackJack.spadesTextures[10];
+            }
+            else if (id == 12) // Q
+            {
+                texture = blackJack.spadesTextures[11];
+            }
+            else if (id == 13) // K
+            {
+                texture = blackJack.spadesTextures[12];
+            }
+
+            if (texture != null)
+            {
+
+                Shader urpShader = Shader.Find("Universal Render Pipeline/Lit");
+
+
+                if (urpShader != null)
+                {
+
+                    Material nuevoMaterial = new Material(urpShader);
+
+
+                    nuevoMaterial.SetTexture("_BaseMap", texture);
+
+
+                    meshRenderer.material = nuevoMaterial;
+                }
+
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- HumbleGamblev2/Assets/South/Scripts/BlackJack/BlackJackScript.cs
+++ HumbleGamblev2/Assets/South/Scripts/BlackJack/BlackJackScript.cs
@@ -22,11 +22,29 @@ public class BlackJackScript : MonoBehaviour
 
     [SerializeField] private InputActionProperty gripAction;
 
+    public Texture2D[] spadesTextures;
+
     // Start is called before the first frame update
     void Start()
     {
         clientCards = new List<BlackJackCardScript>();
         userCards = new List<BlackJackCardScript>();
+
+        spadesTextures = new Texture2D[13]; // 13 texturas, de 0 a 12
+        spadesTextures[0] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_A");  // SP_A = Índice 0
+        spadesTextures[1] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_2");  // SP_2 = Índice 1
+        spadesTextures[2] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_3");  // SP_3 = Índice 2
+        spadesTextures[3] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_4");  // SP_4 = Índice 3
+        spadesTextures[4] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_5");  // SP_5 = Índice 4
+        spadesTextures[5] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_6");  // SP_6 = Índice 5
+        spadesTextures[6] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_7");  // SP_7 = Índice 6
+        spadesTextures[7] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_8");  // SP_8 = Índice 7
+        spadesTextures[8] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_9");  // SP_9 = Índice 8
+        spadesTextures[9] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_10"); // SP_10 = Índice 9
+        spadesTextures[10] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_J"); // SP_J = Índice 10
+        spadesTextures[11] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_Q"); // SP_Q = Índice 11
+        spadesTextures[12] = Resources.Load<Texture2D>("Cards/Types/Spades/SP_K"); // SP_K = Índice 12
+
     }
 
     // Update is called once per frame
@@ -60,7 +78,15 @@ public class BlackJackScript : MonoBehaviour
     {
         GameObject newCardGO = Instantiate(cardPrefab, spawnPosition.position, Quaternion.identity);
         BlackJackCardScript cardScript = newCardGO.GetComponent<BlackJackCardScript>();
-        cardScript.value = RandomValue(1, 11);
+        cardScript.id = RandomValue(1, 13);
+        if (cardScript.id < 11 )
+        {
+            cardScript.value = cardScript.id;
+        }
+        else
+        {
+            cardScript.value = 10;
+        }
         cardScript.type = (CardType)RandomValue(0, 3);
         //currentCardInHand = cardScript;
     }
