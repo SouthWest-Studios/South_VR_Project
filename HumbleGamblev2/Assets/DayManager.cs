2a62ce347a895263813f0bddcf6741afd71f665a
Marti Sabaté <71668554+msabate00@users.noreply.github.com>
2025-06-01 21:40:04 +0200

    end of day updated + money variable

--- HumbleGamblev2/Assets/DayManager.cs
+++ HumbleGamblev2/Assets/DayManager.cs
@@ -1,22 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DayManager : MonoBehaviour
 {
+
+    [Header("References")]
+    public GameObject endDayCanvas;
+    public AudioSource doorBell;
+    public AudioSource alarmWatch;
+
+    public Text totalMoneyTMP;
+    //public Light directionalLight;
+
     [Header("Settings")]
     [Description("En segundos")]
     public float dayTime = 240;
-    public int dayTotal = 4;
+    public int dayTotal = 999;
 
     public int startingMinutesOffset = 17;
+    public Color greenMoneyColor = new Color(18, 255, 75);
+    public Color redMoneyColor = new Color(255, 73, 18);
+
+    //public Color lightNightColor;
 
     [Header("Others")]
     public int daysCounter = 0;
 
+
+    [Header("General Game")]
+    public int money = 100; //-50 -> ESCENA DE PERDIDA
+
+
+
+
+
+
+
+
+
+
+
+
     private float timeCounter = 0;
 
     public static DayManager instance;
+
+    private bool needChangeDay = false;
 
     // Start is called before the first frame update
     private void Awake()
@@ -34,7 +66,7 @@ public class DayManager : MonoBehaviour
 
     void Start()
     {
-        
+        endDayCanvas.SetActive(false);
     }
 
     // Update is called once per frame
@@ -44,16 +76,34 @@ public class DayManager : MonoBehaviour
         if (timeCounter >= dayTime)
         {
             //Fin del dia
-            daysCounter++;
-            if (daysCounter >= dayTotal)
-            {
-                //Pantalla victoria
-            }
-            else
-            {
-                //Recargar
+            if (!needChangeDay) {
+                needChangeDay = true;
+                if (alarmWatch) alarmWatch.Play();
 
-            }
+                if (daysCounter >= dayTotal)
+                {
+                    //Pantalla victoria (bueno se ha quitado al parecer)
+                   
+                }
+                else
+                {
+                    //Recargar
+                    endDayCanvas.SetActive(true);
+                    if (money > 0)
+                    {
+                        totalMoneyTMP.color = greenMoneyColor;
+                        totalMoneyTMP.text = "+" + money.ToString();
+                    }
+                    else
+                    {
+                        totalMoneyTMP.color = redMoneyColor;
+                        totalMoneyTMP.text = money.ToString();
+                    }
+                    
+                    //directionalLight.color = lightNightColor;
+
+                }
+            } 
         }
     }
 
@@ -76,6 +126,27 @@ public class DayManager : MonoBehaviour
 
     public string GetDayString()
     {
-        return daysCounter + "/" + dayTotal;
+        if (daysCounter == 1)
+        {
+            return daysCounter + " day";
+        }
+        else
+        {
+            return daysCounter + " days";
+        }
+            
+    }
+
+    public void GoNextDay()
+    {
+        daysCounter++;
+        needChangeDay = false;
+        timeCounter = 0;
+        endDayCanvas.SetActive(false);
+        if (doorBell)
+        {
+            doorBell.Play();
+        }
+        
     }
 }
